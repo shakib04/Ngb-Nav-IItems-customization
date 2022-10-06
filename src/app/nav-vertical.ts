971c0a5f2cf4abf1f86409922ab2e6cdc9b3dde3
shakib04 <51458806+shakib04@users.noreply.github.com>
2022-10-06 18:54:35 +0600

    style added in nav items

--- src/app/nav-vertical.ts
+++ src/app/nav-vertical.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-nav-vertical',
-  templateUrl: './nav-vertical.html'
+  templateUrl: './nav-vertical.html',
+  styleUrls: ['./nav-vertical.scss']
 })
 export class NgbdNavVertical {
   active = 'top';
