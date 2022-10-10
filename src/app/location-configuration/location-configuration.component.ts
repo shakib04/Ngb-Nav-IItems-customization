73a875ca92d5d2f765a208c59e9db8137fde005e
shakib04 <51458806+shakib04@users.noreply.github.com>
2022-10-10 11:09:09 +0600

    generate component for location config

---
+++ src/app/location-configuration/location-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-location-configuration',
+  templateUrl: './location-configuration.component.html',
+  styleUrls: ['./location-configuration.component.css']
+})
+export class LocationConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/nav-vertical.module.ts
+++ src/app/nav-vertical.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LocationConfigurationComponent } from './location-configuration/location-configuration.component';
 
 import { NgbdNavVertical } from './nav-vertical';
 
 @NgModule({
   imports: [BrowserModule, NgbModule],
-  declarations: [NgbdNavVertical],
+  declarations: [NgbdNavVertical, LocationConfigurationComponent],
   exports: [NgbdNavVertical],
   bootstrap: [NgbdNavVertical]
 })
