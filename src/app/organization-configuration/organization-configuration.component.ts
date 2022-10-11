575eaafdeb37acbe3d15ddb36447f79e7bc92bc4
shakib04 <51458806+shakib04@users.noreply.github.com>
2022-10-12 02:11:18 +0600

    4 files modified, 2 added, 1 deleted

--- src/app/organization-configuration/organization-configuration.component.ts
+++ src/app/organization-configuration/organization-configuration.component.ts
@@ -1,14 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { OrganizationConfigurationService } from './organization-configuration.service';
 
 @Component({
   selector: 'app-organization-configuration',
   templateUrl: './organization-configuration.component.html',
-  styleUrls: ['./organization-configuration.component.css'],
+  styleUrls: ['./organization-configuration.component.scss'],
 })
 export class OrganizationConfigurationComponent implements OnInit {
-  constructor() {}
+  
+
+  selectedFiles:File[] = [];
+
+  editForm = this.fb.group({
+    id: [],
+    name: [null, [Validators.required, Validators.maxLength(255)]],
+    slogan: [null, [Validators.maxLength(255)]],
+    domainName: [null, [Validators.required, Validators.maxLength(255)]],
+    emailAddress: [null, [Validators.required, Validators.maxLength(255)]],
+    hrEmailAddress: [null, [Validators.maxLength(255)]],
+    noReplyEmailAddress: [null, [Validators.maxLength(255)]],
+    contactNumber: [null, [Validators.maxLength(255)]],
+    financeManagerPIN: [null, [Validators.maxLength(255)]],
+    financeManagerSignature: [],
+    svgLogo: [],
+    documentLetterHead: [],
+    hasOrganizationStamp: [],
+    organizationStamp: [],
+    linkedin: [null, [Validators.maxLength(255)]],
+    twitter: [null, [Validators.maxLength(255)]],
+    facebook: [null, [Validators.maxLength(255)]],
+    youtube: [null, [Validators.maxLength(255)]],
+    instagram: [null, [Validators.maxLength(255)]],
+    whatsapp: [null, [Validators.maxLength(255)]],
+  });
+
+  constructor(private fb: FormBuilder, protected organizationConfiguration: OrganizationConfigurationService) {}
 
   ngOnInit() {
     console.log('ee');
   }
+
+  onChange(event: any): void {
+    this.selectedFiles = event.target.files;
+
+    const image: any = event.target.files[0];
+    //this.size = image.size;
+    const fr = new FileReader();
+    fr.onload = () => {
+      const img = new Image();
+
+      img.onload = () => {
+        //this.width = img.width;
+        //this.height = img.height;
+      };
+
+      if (typeof fr.result === 'string') {
+        img.src = fr.result;
+      } // This is the data URL
+    };
+  }
+
+  onSubmit(): void {
+    //this.fileValidationMessage = '';
+    if (this.selectedFiles.length > 0) {
+      alert('Uploading Id Card');
+      this.organizationConfiguration.create(this.selectedFiles).subscribe(() => {
+        alert('saved');
+      });
+    } else {
+      alert('No File Selected')
+    }
+  }
+
 }
--- src/app/organization-configuration/organization-configuration.service.ts
+++ src/app/organization-configuration/organization-configuration.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IOrganization } from './organization-model';
 @Injectable()
 export class OrganizationConfigurationService {
+  public resourceUrl = 'http://localhost:8080' + '/api/configs/organization';
 
-  constructor() { }
+  constructor(protected http: HttpClient) {}
 
+  create(files: File[]): Observable<HttpResponse<IOrganization>> {
+    const organization = {
+      contactNumber: 'string',
+      documentLetterHead: 'string',
+      domainName: 'string',
+      emailAddress: 'string',
+      facebook: 'string',
+      financeManagerPIN: 'string',
+      financeManagerSignature: 'string',
+      hasOrganizationStamp: true,
+      hrEmailAddress: 'string',
+      instagram: 'string',
+      linkedin: 'string',
+      name: 'string',
+      noReplyEmailAddress: 'string',
+      organizationStamp: 'string',
+      slogan: 'string',
+      svgLogo: 'string',
+      twitter: 'string',
+      whatsapp: 'string',
+      youtube: 'string',
+    };
+
+    const formData: FormData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append('file', files[i]);
+    }
+
+    formData.append(
+      'organization',
+      new Blob([JSON.stringify(organization)], { type: 'application/json' })
+    );
+    return this.http.post<IOrganization>(this.resourceUrl, formData, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + btoa('admin:admin'),
+      },
+      observe: 'response',
+    });
+  }
 }
